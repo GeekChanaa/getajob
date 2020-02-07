595fff4f7137939fb07cf791c3ac8166c76f1052
AdnaneAm <enanda.am@gmail.com>
2020-02-07 09:40:59 +0100

    Last

--- resources/views/jobbing/seminars/seminar.blade.php
+++ resources/views/jobbing/seminars/seminar.blade.php
@@ -3,21 +3,24 @@
 
 @section('content')
 
-
-<div class="">
-  <span>title</span> <span>{{$seminar->title}}</span>
-</div>
-<div class="">
-  <span>user_id</span> <span>{{$seminar->user_id}}</span>
-</div>
-<div class="">
-  <span>begin</span> <span>{{$seminar->begin}}</span>
-</div>
-<div class="">
-  <span>end</span> <span>{{$seminar->end}}</span>
-</div>
-<div class="">
-  <span>description</span> <span>{{$seminar->description}}</span>
+<div class="seminar-infos">
+  <div class="seminar-header">
+    <div class="">
+      <span>{{$seminar->title}}</span>
+    </div>
+    <div class="">
+      <span>{{$seminar->description}}</span>
+    </div>
+  </div>
+  <div class="">
+    <span>{{explode($seminar->begin)[Ø]}}</span>
+  </div>
+  <div class="">
+    <span>{{explode($seminar->end)[Ø]}}</span>
+  </div>
+  <div class="">
+    <span>{{$seminar->user_id}}</span>
+  </div>
 </div>
 
 
